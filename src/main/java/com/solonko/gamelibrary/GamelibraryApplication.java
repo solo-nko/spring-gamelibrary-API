7a4cae0909c2d5f8803ddf82a91428a454e7c5fb
nko <solowingrazgriz@outlook.com>
2023-06-11 14:54:28 +0900

    initialize repo

---
+++ src/main/java/com/solonko/gamelibrary/GamelibraryApplication.java
@@ -0,0 +1,13 @@
+package com.solonko.gamelibrary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GamelibraryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GamelibraryApplication.class, args);
+	}
+
+}
